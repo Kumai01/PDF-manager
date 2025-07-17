a3ddf3def048e6cc5d8326bc3c35e5352d399c5a
Kumai <k.hodaifa01@gmail.com>
2025-07-18 01:09:28 +0200

    add(gui): order tab

--- src/pdfm/gui.py
+++ src/pdfm/gui.py
@@ -4,6 +4,7 @@ from tkinter import filedialog, simpledialog, messagebox, StringVar
 from tkinter import ttk # For themed widgets
 
 from pdfm.merge import PDFMergerManager
+from pdfm.order import PDFOrderManager
 
 def run_gui():
     root: Application = Application()
@@ -50,10 +51,11 @@ class Application(tk.Tk):
         self.order_tab.create_wide_button(text="Choose a File", command=self.choose_file_and_order_buttons, row=0)
         self.order_tab.create_label(text=self.file_path_var, row=2)
 
-        self.order_tab.create_wide_button(text="Order", command=lambda : self.order(), row=3)
+        self.order_tab.create_wide_button(text="Order", command=self.order, row=3)
 
     def choose_file_and_order_buttons(self) -> None:
         self.choose_file()
+        self.order : PDFMergerManager = PDFOrderManager(self.file_path_var.get())
         ttk.Button(self.order_tab, text="Reorder all pages", command=self.take_pages_all).grid(row=1, column=0, sticky="wne", columnspan=2)
         ttk.Button(self.order_tab, text="Reorder some pages", command=self.take_pages_some).grid(row=1, column=2, sticky="wne", columnspan=2)
 
@@ -76,20 +78,37 @@ class Application(tk.Tk):
         for file in files:
             merger.add_file(file)
         final_path: str = merger.write_output()
-        messagebox.showinfo("Success", f"Files are merged in: {final_path}")
         files = []
         self.listbox.delete(0, tk.END)
 
-    def order(self):
-        ...
+    def order(self) -> None:
+        if self.new_order is not None:
+            self.order.order_pages(new_order=self.new_order)
+            final_path: str = self.order.write_output()
+            self.success_message(final_path)
+            return
+        if self.pages_together is not None and self.after is not None:
+            self.order.order_pages(pages_together=self.pages_together, before=self.before)
+            final_path: str = self.order.write_output()
+            self.success_message(final_path)
+            return
 
     def take_pages_all(self):
-        order_str = simpledialog.askstring("Page Order", f"Enter new order (1-20), comma-separated):")
-        if not order_str:
-            return
-    def take_pages_some(self):
-        ...
+        order_str = simpledialog.askstring("Page Order", f"Enter new order (1-{self.order.num_pages()}), comma-separated):")
+        ttk.Label(self.order_tab, text=order_str)
         
+        self.new_order = [int(i.strip()) - 1 for i in order_str.split(",")]
+        if any(i < 0 or i >= self.order.num_pages() for i in self.new_order):
+            raise ValueError("Page numbers out of range.")
+        
+
+    def take_pages_some(self):
+        dialog = TwoFieldDialog(self)
+        if dialog.result:
+            self.pages_together, self.before = dialog.result
+            
+    def success_message(self, final_path: str) -> None:
+        messagebox.showinfo("Success", f"Files are merged in: {final_path}")
 
 class Tab(ttk.Frame):
     def __init__(self, parent: tk.Tk|ttk.Frame):
@@ -111,6 +130,22 @@ class Tab(ttk.Frame):
 
     def create_label(self, text: str, row: int) -> None:
         ttk.Label(self, textvariable=text).grid(row=row, column=0, sticky="wne", columnspan=4)
+
+class TwoFieldDialog(simpledialog.Dialog):
+    def body(self, parent: ttk.Frame):
+        tk.Label(parent, text="Pages to drage:").grid(row=0)
+        tk.Label(parent, text="Put the pages after:").grid(row=1)
+
+        self.pages_to_move = tk.Entry(parent)
+        self.before = tk.Entry(parent)
+
+        self.pages_to_move.grid(row=0, column=1)
+        self.before.grid(row=1, column=1)
+
+        return self.pages_to_move  # initial focus
+
+    def apply(self):
+        self.result = (self.pages_to_move.get(), self.before.get())
 
 if __name__ == "__main__":
     run_gui()
